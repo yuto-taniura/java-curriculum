1ee10f036beda04fc49ec7118e1b2a89a4e438f9
yuto-taniura <yuto.taniura@gmail.com>
2024-01-21 08:59:11 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO 自動生成されたメソッド・スタブ
+    }
+}
---
+++ src/question_1/Main.java
@@ -0,0 +1,13 @@
+package question_1;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    String name = "y.taniura";
+    int num = 23;
+    final double PI = 3.14;
+
+    System.out.println("名前は" + name + "です。");
+    System.out.println("好きな数字は" + num + "です。");
+    System.out.println("円周率は" + PI + "です。");
+  }
+}
